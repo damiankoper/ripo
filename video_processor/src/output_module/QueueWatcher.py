b912946246c251434935f7604520b3f61849e616
damiankoper <kopernickk@gmail.com>
2020-03-24 11:38:47 +0100

    input port in settings socketio works with websocket transport method

--- video_processor/src/output_module/QueueWatcher.py
+++ video_processor/src/output_module/QueueWatcher.py
@@ -1,21 +1,28 @@
 from threading import Thread, Lock
 from multiprocessing import Queue
 from .WebsocketServer import WebsocketServer
+import asyncio
 
 
 class QueueWatcher(Thread):
-    def __init__(self, queue: Queue, list: list, lock: Lock, server: WebsocketServer):
+    def __init__(self, queue: Queue, list: list, lock: Lock, server: WebsocketServer, loop):
         Thread.__init__(self)
         self.queue = queue
         self.lock = lock
         self.list = list
         self.server = server
 
+        self.loop = loop
+
     def run(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.create_task(self._run())
+
+    async def _run(self):
         while 1:
             value = self.queue.get()
-            # print(value)
             with self.lock:
                 self.list[:] = value
                 self.server.poolState.balls = self.list
-                self.server.emitPoolState()
+                await self.server.emitPoolState()
+                await self.server.sio.sleep(0)
