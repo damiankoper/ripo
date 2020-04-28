af7bb135fe77e0917875e68802b12350c8ee4fd9
Damian Koper <kopernickk@gmail.com>
2020-04-28 10:15:12 +0200

    Labeler fix/workaround

--- video_processor/src/labeler.py
+++ video_processor/src/labeler.py
@@ -12,11 +12,16 @@ imagePaths = sorted(list(paths.list_images(args.input)))
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', 500, 500)
 
+image = cv2.imread(imagePaths[0])
+cv2.imshow('image', image)
+cv2.waitKey(0)
+
 for img in imagePaths:
+    cv2.waitKey(10)
     print(img)
     image = cv2.imread(img)
     cv2.imshow('image', image)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
     label = input("Label: ")
     basename = os.path.basename(img)
     os.makedirs(args.output+"/"+label+"/", exist_ok=True)
